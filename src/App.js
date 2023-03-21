7be62d2983a65e49a6cabd04a51954e5931c5838
ted <tpprogrammingcontact@gmail.com>
2023-03-21 15:06:40 +0100

    initialisingguessthevogue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const random = Math.floor(Math.random() * (1942 - 1940 + 1)) + 1940;
+
+  const selection = "REACT_APP_" + random
+
+  console.log(selection)
+
   return (
-    <div>
+    <div className="main-title">
       <h1>
-        Hello
+        Guess The Vogue
       </h1>
+      <p>Can you guess what year this issue was published?</p>
+      <div className="image-container">
+      <img src={process.env[selection]}/>
+      </div>
+      
     </div>
   );
 }
